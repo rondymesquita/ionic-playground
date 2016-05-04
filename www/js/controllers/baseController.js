aceb76a46ef07dd380b99e93b3b218d183a656ac
Rondy Mesquita <rondinellimesquitas@gmail.com>
2016-05-03 21:42:20 -0300

    slide tb on going

--- www/js/controllers/baseController.js
+++ www/js/controllers/baseController.js
@@ -1,10 +1,20 @@
-app.controller('BaseController', function($scope, $ionicPlatform, $cordovaBarcodeScanner) {
+app.controller('BaseController', function($scope, $ionicPlatform, $ionicSlideBoxDelegate) {
 
-	$scope.left = function(){
+	$scope.options = {
+		loop: false,
+		effect: 'fade',
+		speed: 500,
+	};
+	$scope.data = {};
+	$scope.$watch('data.slider', function(nv, ov) {
+		$scope.slider = $scope.data.slider;
+	});
+
+	$scope.left = function() {
 		console.log("left");
 	};
 
-	$scope.right = function(){
+	$scope.right = function() {
 		console.log("right");
 	};
 
